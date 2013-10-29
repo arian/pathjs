34823703515d0508775dd77002bb5bc79fd9c2ee
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-10-29 18:27:12 +0100

    Rewrite path.js

--- path.js
+++ path.js
@@ -1,200 +1,214 @@
-var Path = {
-    'version': "0.8.4",
-    'map': function (path) {
-        if (Path.routes.defined.hasOwnProperty(path)) {
-            return Path.routes.defined[path];
-        } else {
-            return new Path.core.route(path);
-        }
-    },
-    'root': function (path) {
-        Path.routes.root = path;
-    },
-    'rescue': function (fn) {
-        Path.routes.rescue = fn;
-    },
-    'history': {
-        'initial':{}, // Empty container for "Initial Popstate" checking variables.
-        'pushState': function(state, title, path){
-            if(Path.history.supported){
-                if(Path.dispatch(path)){
-                    history.pushState(state, title, path);
-                }
-            } else {
-                if(Path.history.fallback){
-                    window.location.hash = "#" + path;
-                }
-            }
-        },
-        'popState': function(event){
-            var initialPop = !Path.history.initial.popped && location.href == Path.history.initial.URL;
-            Path.history.initial.popped = true;
-            if(initialPop) return;
-            Path.dispatch(document.location.pathname);
-        },
-        'listen': function(fallback){
-            Path.history.supported = !!(window.history && window.history.pushState);
-            Path.history.fallback  = fallback;
+"use strict";
 
-            if(Path.history.supported){
-                Path.history.initial.popped = ('state' in window.history);
-                Path.history.initial.URL = location.href;
-                window.onpopstate = Path.history.popState;
-            } else {
-                if(Path.history.fallback){
-                    for(var route in Path.routes.defined){
-                        if(route.charAt(0) != "#"){
-                          Path.routes.defined["#"+route] = Path.routes.defined[route];
-                          Path.routes.defined["#"+route].path = "#"+route;
-                        }
-                    }
-                    Path.listen();
-                }
-            }
+function trimHash(path) {
+    if (path.charAt(0) == '#') {
+        path = path.slice(1);
+    }
+    return path;
+}
+
+var Path = {
+
+    version: "0.8.4",
+
+//    hasPushState: !!window.history.pushState,
+    hasPushState: false,
+
+    map: function (path){
+        var defined = Path.routes.defined;
+        return defined[path] || (defined[path] = new Route(path));
+    },
+
+    root: function (path) {
+        Path.routes.root = path;
+        return this;
+    },
+
+    open: function(path) {
+        if (Path.hasPushState) {
+            history.pushState({}, path, path);
+        } else {
+            location.hash = (path.charAt(0) == '#' ? '' : '#') + path;
         }
     },
-    'match': function (path, parameterize) {
-        var params = {}, route = null, possible_routes, slice, i, j, compare;
-        if (!Path.history.supported && Path.history.fallback && path.charAt(0) != '#') {
-            path = '#' + path;
+
+    popState: function(event){
+        Path.dispatch(location.pathname);
+    },
+
+    listen: function(){
+
+        if (Path.hasPushState){
+
+            // if it's /bar/foo#/mix go to /mix
+            var hash = location.hash.slice(1);
+            if (hash) {
+                Path.open(hash);
+            }
+
+            window.addEventListener('popstate', function(event){
+                Path.popState(event);
+            }, false);
+
+            Path.dispatch(location.pathname);
+
+        } else {
+
+            // from /mix to /mix#/mix
+            if (location.hash) {
+                location.href = trimHash(location.hash);
+            }
+
+            var fn = function() {
+                Path.dispatch(location.hash);
+            };
+
+            // The 'document.documentMode' checks below ensure that PathJS fires the right events
+            // even in IE "Quirks Mode".
+            if ("onhashchange" in window && (!document.documentMode || document.documentMode >= 8)) {
+                window.onhashchange = fn;
+            } else {
+                setInterval(fn, 50);
+            }
+
+            Path.dispatch(location.hash);
+
         }
-        for (route in Path.routes.defined) {
+    },
+
+    match: function(path) {
+        path = trimHash(path);
+
+        var routes = Path.routes.defined;
+
+        for (var route in routes) {
             if (route !== null && route !== undefined) {
-                route = Path.routes.defined[route];
-                possible_routes = route.partition();
-                for (j = 0; j < possible_routes.length; j++) {
-                    slice = possible_routes[j];
-                    compare = path;
-                    if (slice.search(/:/) > 0) {
-                        for (i = 0; i < slice.split("/").length; i++) {
-                            if ((i < compare.split("/").length) && (slice.split("/")[i].charAt(0) === ":")) {
-                                params[slice.split('/')[i].replace(/:/, '')] = compare.split("/")[i];
-                                compare = compare.replace(compare.split("/")[i], slice.split("/")[i]);
-                            }
-                        }
-                    }
-                    if (slice === compare) {
-                        if (parameterize) {
-                            route.params = params;
-                        }
-                        return route;
-                    }
+                route = routes[route];
+
+                var match = route.match(path);
+                if (match){
+                    return {route: route, match: match};
                 }
             }
         }
+
         return null;
     },
-    'dispatch': function (passed_route) {
-        var previous_route, matched_route;
-        if (Path.routes.current !== passed_route) {
-            Path.routes.previous = Path.routes.current;
-            Path.routes.current = passed_route;
-            matched_route = Path.match(passed_route, true);
 
-            if (Path.routes.previous) {
-                previous_route = Path.match(Path.routes.previous);
-                if (previous_route !== null && previous_route.do_exit !== null) {
-                    previous_route.do_exit(matched_route);
+    dispatch: function(path) {
+        path = trimHash(path);
+
+        var routes = Path.routes;
+
+        if (routes.current != path){
+
+            var m = Path.match(path);
+
+            if (m) {
+
+                var match = m.match;
+                var route = m.route;
+
+                var previous = routes.previous;
+                if (previous) {
+                    previous.doExit(routes.params, route);
                 }
-            }
 
-            if (matched_route !== null) {
-                matched_route.run();
-                return true;
-            } else {
-                if (Path.routes.rescue !== null) {
-                    Path.routes.rescue();
-                }
+                route.doEnter(match, routes.previous);
+
+                routes.current = path;
+                routes.params = match;
+                routes.previous = route;
+
+                return;
             }
         }
     },
-    'listen': function () {
-        var fn = function(){
-            Path.dispatch(location.hash);
-        };
 
-        if (location.hash === "") {
-            if (Path.routes.root !== null) {
-                location.hash = Path.routes.root;
-            }
-        }
-
-        // The 'document.documentMode' checks below ensure that PathJS fires the right events
-        // even in IE "Quirks Mode".
-        if ("onhashchange" in window && (!document.documentMode || document.documentMode >= 8)) {
-            window.onhashchange = fn;
-        } else {
-            setInterval(fn, 50);
-        }
-
-        if(location.hash !== "") {
-            Path.dispatch(location.hash);
-        }
-    },
-    'core': {
-        'route': function (path) {
-            this.path = path;
-            this.action = null;
-            this.do_enter = [];
-            this.do_exit = null;
-            this.params = {};
-            Path.routes.defined[path] = this;
-        }
-    },
-    'routes': {
-        'current': null,
-        'root': null,
-        'rescue': null,
-        'previous': null,
-        'defined': {}
+    routes: {
+        params: {},
+        current: null,
+        previous: null,
+        defined: {}
     }
 };
-Path.core.route.prototype = {
-    'to': function (fn) {
-        this.action = fn;
-        return this;
-    },
-    'enter': function (fns) {
-        if (fns instanceof Array) {
-            this.do_enter = this.do_enter.concat(fns);
-        } else {
-            this.do_enter.push(fns);
-        }
-        return this;
-    },
-    'exit': function (fn) {
-        this.do_exit = fn;
-        return this;
-    },
-    'partition': function () {
-        //var parts = [], options = [], re = /\(([^}]+?)\)/g, text, i;
-        //while (text = re.exec(this.path)) {
-        //    parts.push(text[1]);
-        //}
-        //options.push(this.path.split("(")[0]);
-        //for (i = 0; i < parts.length; i++) {
-        //    options.push(options[options.length - 1] + parts[i]);
-        //}
-        return [this.path];
-    },
-    'run': function () {
-        var halt_execution = false, i, result, previous;
 
-        if (Path.routes.defined[this.path].hasOwnProperty("do_enter")) {
-            if (Path.routes.defined[this.path].do_enter.length > 0) {
-                for (i = 0; i < Path.routes.defined[this.path].do_enter.length; i++) {
-                    result = Path.routes.defined[this.path].do_enter[i].apply(this, arguments);
-                    if (result === false) {
-                        halt_execution = true;
-                        break;
-                    }
-                }
-            }
-        }
-        if (!halt_execution) {
-            Path.routes.defined[this.path].action();
-        }
+var toString = Object.prototype.toString;
+
+// Taken from express.js
+// https://github.com/visionmedia/express/blob/9bed2b80eecb9/lib/utils.js#L294-L314
+var pathRegexp = function(path, keys, sensitive, strict) {
+    if (toString.call(path) == '[object RegExp]') return path;
+    if (typeof path != 'string' && path && path.join) path = '(' + path.join('|') + ')';
+    path = path
+        .concat(strict ? '' : '/?')
+        .replace(/\/\(/g, '(?:/')
+        .replace(/(\/)?(\.)?:(\w+)(?:(\(.*?\)))?(\?)?(\*)?/g, function(_, slash, format, key, capture, optional, star){
+            keys.push({
+                name: key,
+                optional: !!optional
+            });
+            slash = slash || '';
+            return '' +
+                (optional ? '' : slash) +
+                '(?:' +
+                (optional ? slash : '') +
+                (format || '') + (capture || (format && '([^/.]+?)' || '([^/]+?)')) + ')' +
+                (optional || '') +
+                (star ? '(/*)?' : '');
+        })
+        .replace(/([\/.])/g, '\\$1')
+        .replace(/\*/g, '(.*)');
+    return new RegExp('^' + path + '$', sensitive ? '' : 'i');
+};
+
+function Route(path) {
+    this._path = path;
+    this._keys = [];
+    this._match = pathRegexp(path, this._keys);
+    this._enter = [];
+    this._exit = [];
+}
+
+Route.prototype.to = function(fn) {
+    if (typeof fn == 'function') {
+        this._enter.push(fn);
+    } else {
+        this._enter = this.enter.concat(fn);
     }
+    return this;
+};
+
+Route.prototype.exit = function(fn) {
+    if (typeof fn == 'function') {
+        this._exit.push(fn);
+    } else {
+        this._exit = this._exit.concat(fn);
+    }
+    return this;
+};
+
+Route.prototype.doEnter = function(params, previous) {
+    for (var i = 0, l = this._enter.length; i < l; i++) {
+        this._enter[i].call(this, params, previous);
+    }
+};
+
+Route.prototype.doExit = function(params, next) {
+    for (var i = 0, l = this._exit.length; i < l; i++) {
+        this._exit[i].call(this, params, next);
+    }
+};
+
+Route.prototype.match = function(path) {
+    if (typeof path != 'string') return false;
+    var match = path.match(this._match);
+    if (!match) return false;
+    var result = {};
+    for (var i = 0; i < this._keys.length; i++) {
+        result[this._keys[i].name] = match[i + 1];
+    }
+    return result;
 };
 
 if (typeof module != 'undefined') {
